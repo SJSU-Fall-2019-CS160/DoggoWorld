8dabbcc9d2d632f5e12cd106f5b896dce1b33f1d
Rick Li <li.rick.1996@gmail.com>
2019-10-16 14:29:50 -0700

    modularize chat function

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,23 @@
 const express = require("express");
 const app = express();
-//const dateTime = require("simple-datetime-formater");
 const bodyParser = require("body-parser");
 const sampleRouter = require("./route/sampleUpdateRoute")
-//require the http module
 const http = require("http").Server(app);
-
-// require the socket.io module
 const io = require("socket.io");
 const socket = io(http);
+const mysql = require('mysql');
+const pool = mysql.createPool({
+    connectionLimit: 100,
+    host: 'localhost',
+    user: 'nodeuser',
+    password: 'nodetest',   // configure to old mysql_native_password
+    database: 'doggoworld',
+    debug: false
+});
+const chatHander = require("./src/chatHandler")(socket, pool);
 
 /-development data-/
 const port = 5000;
-const testUserID = 1; // modify with login cookie data when ready
-const testChatID = 1;
 /----/
 
 //bodyparser middleware
@@ -22,33 +26,11 @@ app.use(bodyParser.json());
 //routes
 app.use("/sample", sampleRouter);
 
-//database handling
-const dbHandler = require('./src/databaseHandler');
-app.get('/data',(req, res)=> dbHandler.query(req,res));
-
 //fetch all chat messages
-app.get('/chats', (req, res) => dbHandler.sendAllChats(req, res, testChatID));
+app.get('/chats', (req, res) => chatHander.sendAllChats(req, res));
 
 //set the express.static middleware
 app.use(express.static(__dirname + "/public"));
-
-//integrating socketio
-socket.on("connection", (socket)=>{
-    console.log("User chat connected");
-    
-    socket.on("disconnect", () => {
-        console.log("Disconnected");
-    });
-    
-    socket.on("chat message", function(msg) {
-        console.log("message: " + msg);
-        socket.broadcast.emit("received",{message: msg})
-        dbHandler.addMessage(msg, testUserID, testChatID); // message, userid, chatid
-    });
-});
-
-
-
 
 // start listening on port
 app.listen(port, () => console.log(`DoggoWorld listening on port ${port}`));
