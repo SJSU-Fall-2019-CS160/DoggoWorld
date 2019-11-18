910cd48af3d426ad8e8c7186fdf1a07a99522900
Hankang0321 <hanjong.kang@sjsu.edu>
2019-11-17 23:22:30 -0800

    fronted

---
+++ frontend/src/components/Profile.js
@@ -0,0 +1,51 @@
+import React, { Component } from "react";
+import { getProfile } from "./UserFunctions";
+
+class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      password: ""
+    };
+  }
+
+  componentDidMount() {
+    getProfile()
+      .then(data =>
+        this.setState({
+          username: data.username,
+          password: data.hashed_password
+        })
+      )
+      .catch(err => {
+        console.log(err.response.data);
+      });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="jumbotron mt-5">
+          <div className="col-sm-8 mx-auto">
+            <h1 className="text-center">PROFILE</h1>
+          </div>
+          <table className="table col-md-6 mx-auto">
+            <tbody>
+              <tr>
+                <td>Username</td>
+                <td>{this.state.username}</td>
+              </tr>
+              <tr>
+                <td>Hashed password (demo purpose)</td>
+                <td>{this.state.password}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Profile;
