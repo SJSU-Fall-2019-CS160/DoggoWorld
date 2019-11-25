6f9d661003f471684ff4cbca40d83336d8cb7aef
Hankang0321 <hanjong.kang@sjsu.edu>
2019-11-24 16:56:52 -0800

    backend & frontend connected

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,8 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
-module.exports.io = io;  // Eager export
+module.exports.io = io; // Eager export
 const socketHandler = require("./modules/socketHandler");
-
 
 require("./startup/db")();
 require("./startup/routes")(app);
@@ -14,5 +13,3 @@ const PORT = 5000;
 http.listen(PORT, () => console.log(`DoggoWorld listening on port ${PORT}`));
 
 io.on("connection", socketHandler);
-
-
