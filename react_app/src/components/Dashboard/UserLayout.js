f178699e2c45f829c617bca91fc54b18a0d37fc0
Hankang0321 <hanjong.kang@sjsu.edu>
2019-12-06 14:09:46 -0800

    Dashboard

---
+++ react_app/src/components/Dashboard/UserLayout.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const links = [
+  {
+    name: "My account",
+    linkTo: "/dashboard"
+  },
+  {
+    name: "User information",
+    linkTo: "/profile"
+  },
+  {
+    name: "Event",
+    linkTo: "/event"
+  }
+];
+
+const UserLayout = props => {
+  const generateLinks = links =>
+    links.map((item, i) => (
+      <Link to={item.linkTo} key={i}>
+        {item.name}
+      </Link>
+    ));
+  return (
+    <div className="container">
+      <div className="user_container">
+        <div className="user_left_nav">
+          <h2>J</h2>
+          <h2>My account</h2>
+          <div className="links">{generateLinks(links)}</div>
+          <br />
+          <div>
+            <h2>Utils</h2>
+            <div className="links">
+              <a href="https://www.amazon.com">Chatroom</a>
+            </div>
+            <div className="links">
+              <a href="https://www.walmart.com">Calender</a>
+            </div>
+          </div>
+        </div>
+        <div className="user_right">{props.children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default UserLayout;
